a02d74d54f21e0efa4bd2cf8b3a0ba69172a0455
Wim van Arkel <warkel@me.com>
2019-02-18 21:50:29 +0100

    Adjusted for various screen sizes
    Up to 600px wide

---
+++ script.js
@@ -0,0 +1,13 @@
+// var firstName = prompt('What is your first name?');
+// var lastName = prompt('What is your last name?');
+// var age = prompt('Waht is your age?');
+
+// var fullName = 'So your name is ' + firstName + ' ' + lastName + ' and you are ' + age + ' old.';
+// alert(fullName);
+// console.log(fullName);
+
+
+//////////////////////////
+// If statement
+
+//var firstNameFirstPerson = prompt('What is your first name?')
